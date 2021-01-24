34fab66f09418d6ab56abf5c3cc000af38505bfc
MyEmailsMessages <77932203+MyEmailsMessages@users.noreply.github.com>
2021-01-24 20:39:09 +0200

    Sending emails in windows From APP C#

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmNewMassege
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.pnlSendEmail = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlSendEmail.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnSend.Location = new System.Drawing.Point(343, 275);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 31);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(24, 20);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(394, 20);
+            this.txtTo.TabIndex = 2;
+            this.txtTo.Text = "To";
+            this.txtTo.Enter += new System.EventHandler(this.txtTo_Enter);
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(24, 43);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(394, 20);
+            this.txtSubject.TabIndex = 4;
+            this.txtSubject.Text = "Subject";
+            this.txtSubject.Enter += new System.EventHandler(this.txtSubject_Enter);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(24, 66);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(394, 155);
+            this.txtContent.TabIndex = 6;
+            // 
+            // pnlSendEmail
+            // 
+            this.pnlSendEmail.Controls.Add(this.label1);
+            this.pnlSendEmail.Controls.Add(this.button5);
+            this.pnlSendEmail.Controls.Add(this.button4);
+            this.pnlSendEmail.Controls.Add(this.button3);
+            this.pnlSendEmail.Controls.Add(this.button2);
+            this.pnlSendEmail.Controls.Add(this.button1);
+            this.pnlSendEmail.Controls.Add(this.txtContent);
+            this.pnlSendEmail.Controls.Add(this.txtSubject);
+            this.pnlSendEmail.Controls.Add(this.txtTo);
+            this.pnlSendEmail.Controls.Add(this.btnSend);
+            this.pnlSendEmail.Location = new System.Drawing.Point(25, 15);
+            this.pnlSendEmail.Name = "pnlSendEmail";
+            this.pnlSendEmail.Size = new System.Drawing.Size(445, 333);
+            this.pnlSendEmail.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button5.Font = new System.Drawing.Font("Webdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button5.Location = new System.Drawing.Point(191, 237);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(28, 31);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "ב";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button4.Location = new System.Drawing.Point(138, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(47, 31);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "abc";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.Location = new System.Drawing.Point(100, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(32, 31);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "U";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Location = new System.Drawing.Point(62, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "I";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(24, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "B";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmNewMassege
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(493, 362);
+            this.Controls.Add(this.pnlSendEmail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmNewMassege";
+            this.Text = "New Massege";
+            this.pnlSendEmail.ResumeLayout(false);
+            this.pnlSendEmail.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Panel pnlSendEmail;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net.Mime;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmNewMassege : Form
+    {
+        public frmNewMassege()
+        {
+            InitializeComponent();
+        }
+        //bold
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtContent.Font = new Font(txtContent.Font.Name, txtContent.Font.Size, FontStyle.Bold);
+        }
+        //Italic
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtContent.Font = new Font(txtContent.Font.Name, txtContent.Font.Size, FontStyle.Italic);
+        }
+        //Underline
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtContent.Font = new Font(txtContent.Font.Name, txtContent.Font.Size, FontStyle.Underline);
+        }
+        //Strikeout
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtContent.Font = new Font(txtContent.Font.Name, txtContent.Font.Size, FontStyle.Strikeout);
+        }
+        //Attachment
+        private void button5_Click(object sender, EventArgs e)
+        {
+          OpenFileDialog open = new OpenFileDialog();
+            open.InitialDirectory = "C://Desktop";
+            //Your opendialog box title name.
+            open.Title = "Select file to be upload.";
+           // which type file format you want to upload in database.just add them.
+           open.Filter = "Select Valid Document(*.pdf; *.doc; *.xlsx; *.html)|*.pdf; *.docx; *.xlsx; *.html";
+           // FilterIndex property represents the index of the filter currently selected in the file dialog box.
+            open.FilterIndex = 1;
+            try
+            {
+                if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    if (open.CheckFileExists)
+                    {
+                        string path = System.IO.Path.GetFullPath(open.FileName);
+                        label1.Text = path;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please Upload document.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            MailMessage msg = new MailMessage("Enter your email", txtTo.Text, txtSubject.Text, txtContent.Text);
+            SendMail(txtSubject.Text, txtContent.Text, "Enter your email", label1.Text);
+
+            MessageBox.Show("The email was sent successfully");
+            txtTo.Text = "To";
+            txtSubject.Text = "Subject";
+            txtContent.Text = "";
+            label1.Text = "";
+        }
+        public void SendMail(string sub, string body, string mailto, string filePath)
+        {
+            MailMessage msg = new MailMessage("Enter your email", txtTo.Text, txtSubject.Text,txtContent.Text);
+            msg.IsBodyHtml = false;
+            msg.BodyEncoding = Encoding.Default;
+            msg.Priority = System.Net.Mail.MailPriority.High;
+            if(filePath != "")
+            msg.Attachments.Add(new Attachment(filePath));
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new NetworkCredential("Enter your email", "Enter your password");
+            client.Timeout = 20000;
+            try
+            {
+                client.Send(msg);
+            }
+            catch 
+            {
+                throw new Exception("Fail Has error");
+            }
+            finally
+            {
+                msg.Dispose();
+            }
+        }
+        private void txtSubject_Enter(object sender, EventArgs e)
+        {
+            txtSubject.Text = "";
+        }
+        private void txtTo_Enter(object sender, EventArgs e)
+        {
+            txtTo.Text = "";
+        }
+    }
+}
